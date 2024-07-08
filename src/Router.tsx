11740fead12433351fdd5d97dbde54c4d1d177d6
LinCie <aldiofernanda@protonmail.com>
2024-07-08 20:03:48 +0700

    added login page

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,7 @@ import {
 import Root from "./Root";
 import Index from "./pages/Index";
 import SignUp from "./pages/SignUp";
+import LogIn from "./pages/LogIn";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -13,6 +14,7 @@ const router = createBrowserRouter(
       <Route path="/" element={<Root />}>
         <Route path="/" element={<Index />} index />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
       </Route>
     </>
   )
---
+++ src/pages/LogIn.tsx
@@ -0,0 +1,138 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import instance from "../lib/instance";
+import { LoginContext, UsernameContext } from "../rootContext";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="/">
+        Diagnosa DBD
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default function LogIn() {
+  const loginContext = React.useContext(LoginContext);
+  const usernameContext = React.useContext(UsernameContext);
+
+  const navigate = useNavigate();
+
+  const [cookies, setCookie, removeCookie] = useCookies();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const response = await instance.post("auth/login", {
+      username: data.get("username"),
+      password: data.get("password"),
+    });
+
+    const accessToken: string = response.data.access_token;
+    setCookie("access_token", accessToken);
+
+    const refreshToken: string = response.data.refresh_token;
+    setCookie("refresh_token", refreshToken);
+
+    const usernameResponse = await instance.get("users/username", {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+
+    const returnedUsername: string = usernameResponse.data.username;
+    usernameContext?.setUsername(returnedUsername);
+
+    loginContext?.setIsLoggedIn(true);
+
+    navigate("/diagnose");
+  };
+
+  return (
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Log In
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Log In
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/signup" variant="body2">
+                Don't have an account? Signup
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <Copyright sx={{ mt: 5 }} />
+    </Container>
+  );
+}
