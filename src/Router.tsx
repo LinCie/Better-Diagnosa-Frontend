09015a1a0bb645125ea5c2e07b9272cc929f9e05
LinCie <aldiofernanda@protonmail.com>
2024-07-08 20:44:25 +0700

    added route

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,6 +7,7 @@ import Root from "./Root";
 import Index from "./pages/Index";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/LogIn";
+import Diagnose from "./pages/Diagnose";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -15,6 +16,7 @@ const router = createBrowserRouter(
         <Route path="/" element={<Index />} index />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/diagnose" element={<Diagnose />} />
       </Route>
     </>
   )
