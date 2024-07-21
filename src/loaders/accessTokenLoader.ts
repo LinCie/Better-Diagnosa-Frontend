c3d35f381fac67843a993287d6762a478911fca1
LinCie <aldiofernanda@protonmail.com>
2024-07-21 13:15:11 +0700

    ensure refresh token exist

--- src/loaders/accessTokenLoader.ts
+++ src/loaders/accessTokenLoader.ts
@@ -1,15 +1,17 @@
 import { refresh } from "../services/auth";
 
 export async function accessTokenLoader() {
-  try {
-    const accessToken = await refresh();
-    if (accessToken) {
-      localStorage.setItem("access_token", accessToken);
+  if (localStorage.getItem("refresh_token")) {
+    try {
+      const accessToken = await refresh();
+      if (accessToken) {
+        localStorage.setItem("access_token", accessToken);
+      }
+      return;
+    } catch (error) {
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("refresh_token");
+      throw error;
     }
-    return;
-  } catch (error) {
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("refresh_token");
-    throw error;
   }
 }
